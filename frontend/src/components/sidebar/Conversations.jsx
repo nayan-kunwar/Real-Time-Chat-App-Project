ccf3050ec272e3e62abc11d01d1d76f95e2b61cb
Nayan Kunwar <nayankunwar678@gmail.com>
2024-03-30 14:26:06 +0530

    Uodated UI

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -3,21 +3,23 @@ import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
-	const { loading, conversations } = useGetConversations();
-	return (
-		<div className='py-2 flex flex-col overflow-auto'>
-			{conversations.map((conversation, idx) => (
-				<Conversation
-					key={conversation._id}
-					conversation={conversation}
-					emoji={getRandomEmoji()}
-					lastIdx={idx === conversations.length - 1}
-				/>
-			))}
+  const { loading, conversations } = useGetConversations();
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
 
-			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
-		</div>
-	);
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
+    </div>
+  );
 };
 export default Conversations;
 
